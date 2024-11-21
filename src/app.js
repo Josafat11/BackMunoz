502fa3a6ac454d247089b55981e1804827390a32
Josafat11 <116617414+Josafat11@users.noreply.github.com>
2024-11-21 12:04:17 -0600

    correcciones yamil

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ import logoRoutes from './routes/Logo.routes.js';
 const listWhite = [
     'http://localhost:3000',  // Frontend en desarrollo
     'https://frontend-alpha-six-22.vercel.app', // Frontend correcto en producción
-    'http://192.168.1.77:5000' //linux en desarrollo
+    'http://192.168.1.77:5000', //linux en desarrollo
+    'http://192.168.101.20:5000' //linux yamil
 ];
 
 const corsOptions = {
